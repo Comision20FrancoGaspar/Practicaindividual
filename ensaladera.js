cc6d846ec4af25c4d1ee7e5ec5f38d9feefa33a4
Comicion20FrancoGaspar <francogaspar.b.s@hotmail.com>
2023-05-22 20:18:34 -0300

    agregar instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log("Aca esta la ensaladera");
+console.log("instrucciones");
+console.log(`
+1. Lavar las verduras
+2. Cortar a cuchillo la lechuca
+3. Cortar el tomate en cuadrados, a 45 grados}
+4. cortar la cebolla a la rodajas
+`)
